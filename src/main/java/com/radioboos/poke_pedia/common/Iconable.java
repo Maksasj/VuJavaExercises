5f696c75116ec36235f362d2cf9eecccacca9fe9
maksasj <radioboos@gmail.com>
2023-12-14 22:26:25 +0200

    little license things

--- src/main/java/com/radioboos/poke_pedia/common/Iconable.java
+++ src/main/java/com/radioboos/poke_pedia/common/Iconable.java
@@ -1,3 +1,8 @@
+/**
+ * @author
+ * Maksim Jaroslavcevas 2 grupe radioboos@gmail.com
+*/
+
 package com.radioboos.poke_pedia.common;
 
 import javafx.scene.image.Image;
--- src/main/java/com/radioboos/poke_pedia/pokemon/PokemonType.java
+++ src/main/java/com/radioboos/poke_pedia/pokemon/PokemonType.java
@@ -1,3 +1,8 @@
+/**
+ * @author
+ * Maksim Jaroslavcevas 2 grupe radioboos@gmail.com
+*/
+
 package com.radioboos.poke_pedia.pokemon;
 
 public enum PokemonType {
